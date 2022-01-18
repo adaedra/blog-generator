26d749ff7106a7ccf076dd6d68b6855fbc675934
Thibault Hamel <contact@adaedra.eu>
2022-01-18 02:15:53 +0100

    Dynamic blog title

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use dolmen::{tag, ElementBox, HtmlDocument, IntoElementBox};
 use pastex::output::html;
 
+#[derive(serde::Deserialize)]
+struct BlogData {
+    title: String,
+}
+
 fn articles() -> anyhow::Result<Vec<ElementBox>> {
-    Ok(glob::glob("../blog-articles/**/*.px")?
+    Ok(glob::glob("../blog-data/articles/**/*.px")?
         .map(|article| {
             let article = article.unwrap();
             let document = pastex::document::process(&article).unwrap();
@@ -20,14 +25,17 @@ fn articles() -> anyhow::Result<Vec<ElementBox>> {
 }
 
 fn main() -> anyhow::Result<()> {
+    let options = std::fs::read_to_string("../blog-data/blog.toml")?;
+    let options: BlogData = toml::from_str(&options)?;
+
     let document = HtmlDocument(tag!(html(lang = "en") {
         tag!(head {
             tag!(meta(charset = "utf-8"));
-            tag!(title { "My blog"; });
+            tag!(title { options.title.clone(); });
         });
         tag!(body {
             tag!(nav {
-                tag!(a(href="/") { "My blog"; });
+                tag!(a(href="/") { options.title; });
             });
 
             tag!(main => articles()?);
