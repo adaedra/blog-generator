c93790e5ef106091c3226393e1772e2ae5d10e98
Thibault Hamel <contact@adaedra.eu>
2022-02-05 21:44:48 +0100

    Good progress
    * Updated index
    * Added article generation
    * More metadata

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,152 @@
 use dolmen::{tag, ElementBox, Fragment, HtmlDocument, IntoElementBox, Tag};
-use pastex::output::html;
-use std::{fs, io::Write, path::Path};
+use pastex::{document::Document, output::html};
+use std::{
+    fs,
+    io::Write,
+    path::{Path, PathBuf},
+};
+
+#[derive(serde::Deserialize)]
+struct Social {
+    name: String,
+    icon_name: String,
+    url: String,
+}
 
 #[derive(serde::Deserialize)]
 struct BlogData {
     title: String,
+    tagline: String,
+    footer: String,
+    socials: Vec<Social>,
 }
 
-fn articles() -> anyhow::Result<Vec<ElementBox>> {
-    Ok(glob::glob("../blog-data/articles/**/*.px")?
-        .map(|article| {
-            let article = article.unwrap();
-            let document = pastex::document::process(&article).unwrap();
+struct Article {
+    document: Document,
+    path: PathBuf,
+}
 
-            let title = document.metadata.title.as_ref().unwrap().to_string();
-            let (document_block, abstract_block) = html::output(&document);
+fn index(options: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
+    let tagline = html::output_fragment(&pastex::document::process_fragment(&options.tagline));
+    let articles = articles
+        .iter()
+        .map(|article| {
+            let title = article
+                .document
+                .metadata
+                .title
+                .as_ref()
+                .unwrap()
+                .to_string();
+            let (_, summary) = html::output(&article.document);
+            let path = article.path.file_stem().unwrap().to_str().unwrap();
 
             tag!(box article {
-                tag!(h1 { &title; });
-                abstract_block.map(|block| tag!(div => block));
-                tag!(div => document_block);
+                tag!(h3 {
+                    tag!(a(href = format!("{}/", path)) { &title; });
+                });
+                summary.map(|block| tag!(div => block));
             })
         })
-        .collect())
+        .collect();
+
+    vec![tag!(box main {
+        tag!(box div(class = "main-wrapper") {
+            tag!(header(class = "home") {
+                tag!(h1 { &options.title; });
+                tag!(p => tagline);
+            });
+        });
+        tag!(box div(class="main-wrapper") {
+            tag!(header {
+                tag!(h2 { "Latest articles"; });
+            });
+            Fragment::from(articles);
+        });
+    })]
+}
+
+fn articles() -> anyhow::Result<Vec<Article>> {
+    glob::glob("../blog-data/articles/**/*.px")?
+        .map(|path| path.map_err(Into::into))
+        .map(|path| {
+            path.and_then(|path| {
+                let document = pastex::document::process(&path)?;
+
+                Ok(Article { document, path })
+            })
+        })
+        .collect()
+}
+
+fn article_page(article: &Article) -> Vec<ElementBox> {
+    let title = article
+        .document
+        .metadata
+        .title
+        .as_ref()
+        .unwrap()
+        .to_string();
+    let (contents, summary) = html::output(&article.document);
+
+    let inner = vec![tag!(box header { tag!(h1 { &title; }); } )]
+        .into_iter()
+        .chain(
+            summary
+                .map(|summary| tag!(box div(class = "abstract") => summary))
+                .into_iter(),
+        )
+        .chain(contents)
+        .collect();
+
+    vec![tag!(box main(class = "main-wrapper") => inner)]
 }
 
 fn layout(options: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
+    let footer = html::output_fragment(&pastex::document::process_fragment(&options.footer));
+    let socials = options
+        .socials
+        .iter()
+        .map(|social| {
+            tag!(box a(href = social.url, target = "_blank", title = social.name) {
+                tag!(svg(xmlns = "http://www.w3.org/2000/svg", viewbox = "0 0 30 30", alt = social.name) {
+                    tag!(r#use(href = format!("/assets/icons.svg#{}", social.icon_name)));
+                });
+                tag!(span { &social.name; });
+            })
+        })
+        .collect();
+
     tag!(html(lang = "en") {
         tag!(head {
             tag!(meta(charset = "utf-8"));
+            tag!(meta(name = "viewport", content = "width=device-width, initial-scale=1"));
+
             tag!(title { &options.title; });
+            // Keep only for production. In development, we use webpack.
+            // TODO: Use a manifest system to have versioned file name.
+            // tag!(link(rel = "stylesheet", type = "text/css", href = "/assets/main.css"));
         });
         tag!(body {
             tag!(nav {
-                tag!(a(href = "/") { &options.title; });
+                tag!(div(class = "wrapper") {
+                    tag!(a(href = "/") { &options.title; });
+                    tag!(a(href = "/articles/") { "Articles"; });
+                    tag!(a(href = "/about/") { "About me"; });
+                    tag!(span(class = "separator") { Fragment::empty(); });
+                    Fragment::from(socials);
+                });
+            });
+            tag!(div(class = "header-picture") {
+                Fragment::empty();
             });
 
-            tag!(main { inner; });
+            inner;
+
+            tag!(footer => footer);
+
+            // TODO: Remove for production.
+            tag!(script(type = "text/javascript", src="/assets/main.js") { Fragment::empty(); });
         });
     })
 }
@@ -50,9 +160,24 @@ fn main() -> anyhow::Result<()> {
         fs::create_dir(output_dir)?;
     }
 
-    let document = HtmlDocument(layout(&options, Fragment::from(articles()?)));
-    let mut output = fs::File::create(output_dir.join("index.html"))?;
-    writeln!(output, "{}", document)?;
+    let articles = articles()?;
+
+    {
+        let document = HtmlDocument(layout(&options, Fragment::from(index(&options, &articles))));
+        let mut output = fs::File::create(output_dir.join("index.html"))?;
+        writeln!(output, "{}", document)?;
+    }
+
+    for article in articles {
+        let document = HtmlDocument(layout(&options, Fragment::from(article_page(&article))));
+        let path = output_dir.join(article.path.file_stem().unwrap());
+        if !path.is_dir() {
+            fs::create_dir(&path)?;
+        }
+
+        let mut output = fs::File::create(path.join("index.html"))?;
+        writeln!(output, "{}", document)?;
+    }
 
     Ok(())
 }
