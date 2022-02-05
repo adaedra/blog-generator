5859b2048f582932bb2391c1db770ad7a7489331
Thibault Hamel <contact@adaedra.eu>
2022-02-05 23:53:25 +0100

    Prepare for production build
    * Read webpack manifest for assets
    * Production mode toggle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use clap::Parser;
 use dolmen::{tag, ElementBox, Fragment, HtmlDocument, IntoElementBox, Tag};
 use pastex::{document::Document, output::html};
 use std::{
+    collections::HashMap,
     fs,
     io::Write,
     path::{Path, PathBuf},
@@ -21,13 +23,54 @@ struct BlogData {
     socials: Vec<Social>,
 }
 
+#[derive(Parser)]
+struct CliOptions {
+    #[clap(long)]
+    production: bool,
+    #[clap(long)]
+    manifest: Option<PathBuf>,
+}
+
+trait AssetManifest {
+    fn asset(&self, name: &str) -> String;
+}
+
+struct NoopAssetManifest;
+
+impl AssetManifest for NoopAssetManifest {
+    fn asset(&self, name: &str) -> String {
+        format!("/assets/{}", name)
+    }
+}
+
+struct WebpackManifest(HashMap<String, String>);
+
+impl WebpackManifest {
+    fn from(path: &Path) -> anyhow::Result<WebpackManifest> {
+        let reader = fs::File::open(path)?;
+        let map = serde_json::from_reader(reader)?;
+
+        Ok(WebpackManifest(map))
+    }
+}
+
+impl AssetManifest for WebpackManifest {
+    fn asset(&self, name: &str) -> String {
+        self.0
+            .get(name)
+            .map(Clone::clone)
+            // TODO: Warn of missing asset
+            .unwrap_or_else(|| format!("/assets/{}", name))
+    }
+}
+
 struct Article {
     document: Document,
     path: PathBuf,
 }
 
-fn index(options: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
-    let tagline = html::output_fragment(&pastex::document::process_fragment(&options.tagline));
+fn index(blog_data: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
+    let tagline = html::output_fragment(&pastex::document::process_fragment(&blog_data.tagline));
     let articles = articles
         .iter()
         .map(|article| {
@@ -43,7 +86,7 @@ fn index(options: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
 
             tag!(box article {
                 tag!(h3 {
-                    tag!(a(href = format!("{}/", path)) { &title; });
+                    tag!(a(href = format!("/{}/", path)) { &title; });
                 });
                 summary.map(|block| tag!(div => block));
             })
@@ -53,7 +96,7 @@ fn index(options: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
     vec![tag!(box main {
         tag!(box div(class = "main-wrapper") {
             tag!(header(class = "home") {
-                tag!(h1 { &options.title; });
+                tag!(h1 { &blog_data.title; });
                 tag!(p => tagline);
             });
         });
@@ -102,15 +145,20 @@ fn article_page(article: &Article) -> Vec<ElementBox> {
     vec![tag!(box main(class = "main-wrapper") => inner)]
 }
 
-fn layout(options: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
-    let footer = html::output_fragment(&pastex::document::process_fragment(&options.footer));
-    let socials = options
+fn layout(
+    blog_data: &BlogData,
+    options: &CliOptions,
+    manifest: &dyn AssetManifest,
+    inner: Fragment,
+) -> Tag<dolmen::html::html> {
+    let footer = html::output_fragment(&pastex::document::process_fragment(&blog_data.footer));
+    let socials = blog_data
         .socials
         .iter()
         .map(|social| {
             tag!(box a(href = social.url, target = "_blank", title = social.name) {
                 tag!(svg(xmlns = "http://www.w3.org/2000/svg", viewbox = "0 0 30 30", alt = social.name) {
-                    tag!(r#use(href = format!("/assets/icons.svg#{}", social.icon_name)));
+                    tag!(r#use(href = format!("{}#{}", manifest.asset("header.png"), social.icon_name)));
                 });
                 tag!(span { &social.name; });
             })
@@ -122,15 +170,14 @@ fn layout(options: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
             tag!(meta(charset = "utf-8"));
             tag!(meta(name = "viewport", content = "width=device-width, initial-scale=1"));
 
-            tag!(title { &options.title; });
+            tag!(title { &blog_data.title; });
             // Keep only for production. In development, we use webpack.
-            // TODO: Use a manifest system to have versioned file name.
-            // tag!(link(rel = "stylesheet", type = "text/css", href = "/assets/main.css"));
+            options.production.then(|| tag!(link(rel = "stylesheet", type = "text/css", href = manifest.asset("main.css"))));
         });
         tag!(body {
             tag!(nav {
                 tag!(div(class = "wrapper") {
-                    tag!(a(href = "/") { &options.title; });
+                    tag!(a(href = "/") { &blog_data.title; });
                     tag!(a(href = "/articles/") { "Articles"; });
                     tag!(a(href = "/about/") { "About me"; });
                     tag!(span(class = "separator") { Fragment::empty(); });
@@ -145,15 +192,22 @@ fn layout(options: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
 
             tag!(footer => footer);
 
-            // TODO: Remove for production.
-            tag!(script(type = "text/javascript", src="/assets/main.js") { Fragment::empty(); });
+            (!options.production).then(|| tag!(script(type = "text/javascript", src = manifest.asset("main.js")) { Fragment::empty(); }));
         });
     })
 }
 
 fn main() -> anyhow::Result<()> {
-    let options = fs::read_to_string("../blog-data/blog.toml")?;
-    let options: BlogData = toml::from_str(&options)?;
+    let blog_data = fs::read_to_string("../blog-data/blog.toml")?;
+    let blog_data: BlogData = toml::from_str(&blog_data)?;
+    let options = CliOptions::parse();
+    let manifest = options
+        .manifest
+        .as_ref()
+        .map(|path| WebpackManifest::from(&path))
+        .transpose()?
+        .map(|manifest| Box::new(manifest) as Box<dyn AssetManifest>)
+        .unwrap_or_else(|| Box::new(NoopAssetManifest));
 
     let output_dir = Path::new("output");
     if !output_dir.is_dir() {
@@ -163,13 +217,23 @@ fn main() -> anyhow::Result<()> {
     let articles = articles()?;
 
     {
-        let document = HtmlDocument(layout(&options, Fragment::from(index(&options, &articles))));
+        let document = HtmlDocument(layout(
+            &blog_data,
+            &options,
+            manifest.as_ref(),
+            Fragment::from(index(&blog_data, &articles)),
+        ));
         let mut output = fs::File::create(output_dir.join("index.html"))?;
         writeln!(output, "{}", document)?;
     }
 
     for article in articles {
-        let document = HtmlDocument(layout(&options, Fragment::from(article_page(&article))));
+        let document = HtmlDocument(layout(
+            &blog_data,
+            &options,
+            manifest.as_ref(),
+            Fragment::from(article_page(&article)),
+        ));
         let path = output_dir.join(article.path.file_stem().unwrap());
         if !path.is_dir() {
             fs::create_dir(&path)?;
