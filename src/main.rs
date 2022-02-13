e506ebdae4eaf983a922a24f1fbc69e0f461b810
Thibault Hamel <contact@adaedra.eu>
2022-02-13 19:29:25 +0100

    Missing pieces
    * Added article list
    * Some style updates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use dolmen::{tag, ElementBox, Fragment, HtmlDocument, IntoElementBox, Tag};
+use once_cell::sync::Lazy;
 use pastex::{document::Document, output::html};
 use std::{
     fs,
     io::Write,
+    iter,
     path::{Path, PathBuf},
 };
-use time::Date;
+use time::{format_description::FormatItem, Date};
+
+static DATE_FORMAT: Lazy<Vec<FormatItem<'_>>> =
+    Lazy::new(|| time::format_description::parse("[year]-[month]-[day]").unwrap());
 
 #[derive(serde::Deserialize)]
 struct Social {
@@ -30,41 +35,42 @@ struct Article {
     date: Date,
 }
 
-fn index(blog_data: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
-    let date_format = time::format_description::parse("[year]-[month]-[day]").unwrap();
-    let tagline = html::output_fragment(&pastex::document::process_fragment(&blog_data.tagline));
-    let articles = articles
-        .iter()
-        .rev()
-        .map(|article| {
-            let title = article
-                .document
-                .metadata
-                .title
-                .as_ref()
-                .unwrap()
-                .to_string();
-            let (_, summary) = html::output(&article.document);
-            let path = article.path.file_stem().unwrap().to_str().unwrap();
+fn separator() -> ElementBox {
+    tag!(box p(class = "bl-separator", role = "presentation") { "\u{25C7}"; })
+}
 
-            tag!(box article(class = "article-preview") {
-                tag!(a(href = format!("/{:04}/{:02}/{}/", article.date.year(), article.date.iso_week(), path)) {
-                    tag!(p { &article.date.format(&date_format).unwrap(); });
-                    tag!(h3 { &title; });
-                });
-                summary.map(|block| tag!(div => block));
-            })
-        })
-        .collect();
+fn article_preview(article: &Article) -> ElementBox {
+    let title = article
+        .document
+        .metadata
+        .title
+        .as_ref()
+        .unwrap()
+        .to_string();
+    let (_, summary) = html::output(&article.document);
+    let path = article.path.file_stem().unwrap().to_str().unwrap();
+
+    tag!(box article(class = "bl-article-preview") {
+        tag!(a(href = format!("/{:04}/{:02}/{}/", article.date.year(), article.date.iso_week(), path)) {
+            tag!(p { &article.date.format(&DATE_FORMAT).unwrap(); });
+            tag!(h3 { &title; });
+        });
+        summary.map(|block| tag!(div => block));
+    })
+}
+
+fn index(blog_data: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
+    let tagline = html::output_fragment(&pastex::document::process_fragment(&blog_data.tagline));
+    let articles = articles.iter().rev().map(article_preview).collect();
 
     vec![tag!(box main {
-        tag!(box div(class = "main-wrapper") {
-            tag!(header(class = "home") {
+        tag!(box div(class = "bl-main-wrapper") {
+            tag!(header(class = "bl-home") {
                 tag!(h1 { &blog_data.title; });
                 tag!(p => tagline);
             });
         });
-        tag!(box div(class="main-wrapper") {
+        tag!(box div(class="bl-main-wrapper") {
             tag!(header {
                 tag!(h2 { "Latest articles"; });
             });
@@ -74,8 +80,6 @@ fn index(blog_data: &BlogData, articles: &[Article]) -> Vec<ElementBox> {
 }
 
 fn articles() -> anyhow::Result<Vec<Article>> {
-    let date_format = time::format_description::parse("[year]-[month]-[day]")?;
-
     let articles = glob::glob("../blog-data/articles/**/*.px")?
         .map(|path| path.map_err(Into::into))
         .map(|path| {
@@ -91,7 +95,7 @@ fn articles() -> anyhow::Result<Vec<Article>> {
         .filter(|(document, _)| !document.metadata.draft && document.metadata.date.is_some())
         .map(|(document, path)| Article {
             path,
-            date: Date::parse(document.metadata.date.as_ref().unwrap(), &date_format).unwrap(),
+            date: Date::parse(document.metadata.date.as_ref().unwrap(), &DATE_FORMAT).unwrap(),
             document,
         })
         .collect();
@@ -101,7 +105,6 @@ fn articles() -> anyhow::Result<Vec<Article>> {
 }
 
 fn article_page(article: &Article) -> Vec<ElementBox> {
-    let date_format = time::format_description::parse("[year]-[month]-[day]").unwrap();
     let title = article
         .document
         .metadata
@@ -112,19 +115,19 @@ fn article_page(article: &Article) -> Vec<ElementBox> {
     let (contents, summary) = html::output(&article.document);
 
     let inner = vec![tag!(box header {
-        tag!(p { &article.date.format(&date_format).unwrap(); });
+        tag!(p { &article.date.format(&DATE_FORMAT).unwrap(); });
         tag!(h1 { &title; });
     } )]
     .into_iter()
     .chain(
         summary
-            .map(|summary| tag!(box div(class = "abstract") => summary))
+            .map(|summary| tag!(box div(class = "bl-abstract") => summary.into_iter().chain(iter::once(separator())).collect()))
             .into_iter(),
     )
     .chain(contents)
     .collect();
 
-    vec![tag!(box main(class = "main-wrapper") => inner)]
+    vec![tag!(box main(class = "bl-main-wrapper") => inner)]
 }
 
 fn layout(blog_data: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
@@ -157,23 +160,28 @@ fn layout(blog_data: &BlogData, inner: Fragment) -> Tag<dolmen::html::html> {
         });
         tag!(body {
             tag!(nav {
-                tag!(div(class = "wrapper") {
+                tag!(div(class = "bl-wrapper") {
                     tag!(a(href = "/") { &blog_data.title; });
                     tag!(a(href = "/articles/") { "Articles"; });
-                    tag!(a(href = "/about/") { "About me"; });
-                    tag!(span(class = "separator") { Fragment::empty(); });
+                    tag!(a(href = "/me/") { "About me"; });
+                    tag!(span(class = "bl-separator") { Fragment::empty(); });
                     Fragment::from(socials);
                 });
-            });
-            tag!(div(class = "header-picture") {
-                Fragment::empty();
             });
 
             inner;
 
-            tag!(footer => footer);
+            tag!(footer {
+                tag!(div(class = "bl-wrapper") => iter::once(separator()).chain(footer).collect());
+            });
         });
     })
+}
+
+fn article_list(articles: &[Article]) -> Vec<ElementBox> {
+    vec![
+        tag!(box div(class = "bl-main-wrapper") => articles.iter().rev().map(article_preview).collect()),
+    ]
 }
 
 fn main() -> anyhow::Result<()> {
@@ -196,6 +204,18 @@ fn main() -> anyhow::Result<()> {
         writeln!(output, "{}", document)?;
     }
 
+    {
+        let article_list = article_list(&articles);
+        let article_list = layout(&blog_data, Fragment::from(article_list));
+
+        let path = output_dir.join("articles");
+        if !path.is_dir() {
+            fs::create_dir(&path)?;
+        }
+
+        fs::write(path.join("index.html"), article_list.to_string())?;
+    }
+
     for article in articles {
         let document = HtmlDocument(layout(&blog_data, Fragment::from(article_page(&article))));
         let path = output_dir
@@ -209,8 +229,32 @@ fn main() -> anyhow::Result<()> {
             fs::create_dir_all(&path)?;
         }
 
-        let mut output = fs::File::create(path.join("index.html"))?;
-        writeln!(output, "{}", document)?;
+        fs::write(path.join("index.html"), document.to_string())?;
+    }
+
+    let pages = glob::glob("../blog-data/pages/**/*.px")?
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|path| {
+            let document = pastex::document::process(&path).unwrap();
+            let (result, _) = html::output(&document);
+            let page = tag!(box main(class = "bl-main-wrapper") {
+                tag!(header {
+                    tag!(h1 { &document.metadata.title.unwrap(); });
+                });
+                Fragment::from(result);
+            });
+            let page = layout(&blog_data, Fragment::from(vec![page]));
+
+            (path, page)
+        });
+    for (src_path, page) in pages {
+        let path = output_dir.join(src_path.file_stem().unwrap());
+        if !path.is_dir() {
+            fs::create_dir_all(&path)?;
+        }
+
+        fs::write(path.join("index.html"), page.to_string())?;
     }
 
     Ok(())
